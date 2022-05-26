5f20c8479f9949a795af82ede46ec20dacde6fab
joao augusto <89113486+JADaniel15@users.noreply.github.com>
2022-05-26 00:44:15 -0300

    Add files via upload

---
+++ Java/sintaxe-variaveis-e-fluxo/src/MultpliposDeTres.java
@@ -0,0 +1,13 @@
+
+public class MultpliposDeTres {
+
+	public static void main(String[] args) {
+		 for (int i = 3; i < 100; i+=3 ){
+	            if (i % 3 == 0)    {
+	                System.out.println(i);
+	            }
+	        }
+
+	}
+
+}
---
+++ Java/sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,20 @@
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		 int primeiro = 5;
+		 int segundo = 7;
+		 segundo = primeiro;
+		 primeiro = 10;
+		 
+		 System.out.println(segundo);
+		 System.out.println(primeiro);
+		 
+		 String saudacao = "Ola, meu nome é ";
+		 String nome = "Romulo ";
+		 String continuacao = "e minha idade e? ";
+		 int idade = 100;
+		 System.out.println(saudacao+nome+continuacao+idade);
+	}
+
+}
